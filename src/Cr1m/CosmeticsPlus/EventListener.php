49c7fd37c5fecd8b90b2a4c145679bc7c33448a1
Ben <xzbennzx@gmail.com>
2021-04-28 15:32:35 +1000

    Update EventListener.php

--- src/Cr1m/CosmeticsPlus/EventListener.php
+++ src/Cr1m/CosmeticsPlus/EventListener.php
@@ -15,8 +15,8 @@ class EventListener implements Listener
     public $plugin;
 
     public function __construct(Main $plugin)
-    {
 
+    {
         $this->plugin = $plugin;
         $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
     }
