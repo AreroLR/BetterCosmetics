805df618847dd271f0839621e15d5d5d5a607da5
Ben <xzbennzx@gmail.com>
2021-04-29 09:46:58 +1000

    some basic vanilla pets

---
+++ src/Cr1m/CosmeticsPlus/Pets.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Cr1m\CosmeticsPlus;
+
+use Cr1m\CosmeticsPlus\Main;
+
+use pocketmine\entity\{Entity, EntityIds};
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\network\mcpe\protocol\RemoveActorPacket;
+use pocketmine\Player;
+
+trait Pets
+{
+
+    public function setPet(Player $player, int $type)
+    {
+        $this->delPet($player);
+        $this->pet_basic[$player->getName()]["Count"] = Entity::$entityCount++;
+        $pk = new AddActorPacket();
+        $pk->type = $type;
+        $pk->entityRuntimeId = $this->pet_basic[$player->getName()]["Count"];
+        $pk->position = $player->asVector3();
+        $pk->yaw = $player->getYaw();
+        $pk->pitch = $player->getPitch();
+        $player->getServer()->broadcastPacket([$player], $pk);
+    }
+
+    public function setPetID(Player $pl, int $int): string
+    {
+        switch ($int) {
+            case EntityIds::CHICKEN:
+                $this->setPet($pl, EntityIds::CHICKEN);
+                return "Chicken";
+                break;
+            case EntityIds::RABBIT:
+                $this->setPet($pl, EntityIds::RABBIT);
+                return "Rabbit";
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function removePet(Player $player)
+    {
+        if (isset($this->pet_basic[$player->getName()])) {
+            $pk = new RemoveActorPacket();
+            $pk->entityUniqueId = $this->pet_basic[$player->getName()]["Count"];
+            $player->sendDataPacket($pk);
+            $player->getServer()->broadcastPacket([$player], $pk);
+            unset($this->pet_basic[$player->getName()]);
+        }
+    }
+}
