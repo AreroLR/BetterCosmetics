b8d8b5458734a9561df3c46559da5eca7d6a81ec
Ben <xzbennzx@gmail.com>
2021-04-28 15:23:46 +1000

    Update RainParticles.php

--- src/Cr1m/CosmeticsPlus/Particles/RainParticles.php
+++ src/Cr1m/CosmeticsPlus/Particles/RainParticles.php
@@ -5,10 +5,10 @@ namespace Cr1m\CosmeticsPlus\Particles;
 use pocketmine\math\Vector3;
 use pocketmine\level\particle\{GenericParticle, Particle};
 
-class EndParticles extends GenericParticle
+class RainParticles extends GenericParticle
 {
     public function __construct(Vector3 $pos)
     {
-        parent::__construct($pos, Particle::TYPE_RAIN_SPLASH);
+        parent::__construct($pos, Particle::TYPE_DRIP_WATER);
     }
 }
