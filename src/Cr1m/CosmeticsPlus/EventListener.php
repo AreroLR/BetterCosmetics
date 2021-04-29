ab897dbe3f77e8986c8ea2a70c8139abb76394a4
Ben <xzbennzx@gmail.com>
2021-04-29 10:37:56 +1000

    Update EventListener.php

--- src/Cr1m/CosmeticsPlus/EventListener.php
+++ src/Cr1m/CosmeticsPlus/EventListener.php
@@ -10,6 +10,8 @@ use Cr1m\CosmeticsPlus\Forms\ModalForm;
 use Cr1m\CosmeticsPlus\Forms\SimpleForm;
 use Cr1m\CosmeticsPlus\Forms\CustomForm;
 
+use pocketmine\entity\Entity;
+use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 
@@ -66,6 +68,72 @@ class EventListener implements Listener
         $form->addButton("§l§fMINI ME");
         $form->sendToPlayer($pl);
 
+    }
+
+    public function petOptionHuman(Player $pl): void
+    {
+        $form = $this->plugin->createSimpleForm(function (Player $player, ?int $data) {
+            if (!is_null($data)) {
+                switch ($data) {
+
+                    case 0:
+                        $this->listHumanPet($player);
+                        break;
+
+                    case 1:
+                        $this->plugin->removePetUIT($player);
+                        break;
+                    default:
+                        return;
+
+                }
+            }
+        });
+
+        $form->setTitle("§6§lMINI ME");
+        $form->addButton("§l§fEQUIP");
+        $form->addButton("§l§cUNEQUIP");
+        $form->sendToPlayer($pl);
+
+    }
+
+    public function listHumanPet(Player $pl)
+    {
+
+        $form = $this->plugin->createSimpleForm(function (Player $pl, ?int $data) {
+
+            if (!is_null($data)) {
+
+                $all = $this->plugin->onlyHuman[$pl->getName()] ?? "value";
+
+
+                $vote = $all[$data];
+
+                $online = Core::getInstance()->getServer()->getPlayer($vote);
+                if ($online != null) {
+                    $this->plugin->removePetUIT($pl);
+                    $this->plugin->addUserPet($pl, "human");
+                    $nbt = Entity::createBaseNBT(new Vector3($pl->x, $pl->y + 0.3, $pl->z), null, 88, 6);
+                    $skinTag = $online->namedtag->getCompoundTag("Skin");
+                    assert($skinTag !== null);
+                    $nbt->setTag(clone $skinTag);
+                    $entity = Entity::createEntity("Pet", $pl->getLevel(), $nbt);
+                    $entity->setScale(0.5);
+                    $entity->setScoreTag($online->getName() . "§6's pet");
+                    $entity->setOwningEntity($pl);
+                    $entity->spawnToAll();
+
+                    $pl->sendMessage("§a(!) Successfully spawned pet!");
+
+                } else {
+                    $pl->sendMessage("§c(!) Your pet was unequipped.");
+
+                       }
+                if (isset($this->plugin->onlyHuman[$pl->getName()])) {
+                    unset($this->plugin->onlyHuman[$pl->getName()]);
+                }
+            }
+        });
     }
 }
 
